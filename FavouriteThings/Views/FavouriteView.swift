49c29cebd626571c5c6a23a1ed3779a3cdebb50c
Hannu Packalen <hpackalen@lcs.on.ca>
2024-12-02 08:30:43 -0500

    Made the view a bit more simple

--- FavouriteThings/Views/FavouriteView.swift
+++ FavouriteThings/Views/FavouriteView.swift
@@ -18,12 +18,9 @@ struct FavouriteView: View {
                 .scaledToFill()
                 .frame(width: 100, height: 100)
                 .clipped()
-            VStack{
                 Text(providedFavourite.name)
                     .font(.title).fontWeight(.bold)
-                Text(providedFavourite.reason)
-                    .font(.custom("Small Text", fixedSize: 12))
-            }
+                    .padding()
         }
     }
 }
